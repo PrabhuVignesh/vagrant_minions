0d81de597de5da2836463f90dbbf3dd9e1d5de5d
PrabhuVignesh <prabhu.vignesh1990@gmail.com>
2017-05-03 16:15:18 +0530

    Microservices minion on board

---
+++ microservices_cluster/app/services/bookings.py
@@ -0,0 +1,40 @@
+from services import root_dir, nice_json
+from flask import Flask
+import json
+from werkzeug.exceptions import NotFound
+import os
+
+
+app = Flask(__name__)
+pwd = os.getcwd()
+print pwd
+with open(pwd+"/database/bookings.json".format(root_dir()), "r") as f:
+    bookings = json.load(f)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return nice_json({
+        "uri": "/",
+        "subresource_uris": {
+            "bookings": "/bookings",
+            "booking": "/bookings/<username>"
+        }
+    })
+
+
+@app.route("/bookings", methods=['GET'])
+def booking_list():
+    return nice_json(bookings)
+
+
+@app.route("/bookings/<username>", methods=['GET'])
+def booking_record(username):
+    if username not in bookings:
+        raise NotFound
+
+    return nice_json(bookings[username])
+
+if __name__ == "__main__":
+    app.run(port=5003, debug=True)
+
---
+++ microservices_cluster/app/services/movies.py
@@ -0,0 +1,43 @@
+from services import root_dir, nice_json
+from flask import Flask
+from werkzeug.exceptions import NotFound
+import json
+import os
+
+app = Flask(__name__)
+
+pwd = os.getcwd()
+print pwd
+with open(pwd+"/database/movies.json".format(root_dir()), "r") as f:
+    movies = json.load(f)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return nice_json({
+        "uri": "/",
+        "subresource_uris": {
+            "movies": "/movies",
+            "movie": "/movies/<id>"
+        }
+    })
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def movie_info(movieid):
+    if movieid not in movies:
+        raise NotFound
+
+    result = movies[movieid]
+    result["uri"] = "/movies/{}".format(movieid)
+
+    return nice_json(result)
+
+
+@app.route("/movies", methods=['GET'])
+def movie_record():
+    return nice_json(movies)
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
+
